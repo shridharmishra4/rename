3cedc04cee806780b164e5b3f0d01bc563c66ba2
shridharmishra4 <shridharmishra4@gmail.com>
2013-10-21 01:49:13 +0530

    sub directory support added

--- mutagen/untitled.py
+++ mutagen/untitled.py
@@ -29,6 +29,12 @@ import Tkinter
 import tkFileDialog
 import glob
 from mutagen import id3
+from glob import glob
+from mutagen import *
+
+
+
+
 #from mutagen.easyid3 import EasyID3
 #print EasyID3.valid_keys.keys()
 def Commondialog():
@@ -47,38 +53,55 @@ def Commondialog():
                 
                 
 Commondialog()
-                 
-filenames=glob.glob("*.mp3")
-#for files in filenames:
-    #print files
-    #filenames.append(files)
-    
+files = []
+filenames = []
+start_dir = os.getcwd()
+pattern   = "*.mp3"
+
+for dir ,_,_ in os.walk(start_dir):
+	files.extend(glob(os.path.join(dir,pattern)))
+for file in files:
+	filenames.append(os.path.basename(file))
+
+files.sort()
+#filenames.sort()
+print filenames
+
+
+print """ 
+         Number of songs in the folder ==> %s""" %(len(files))
+
+	
+
+
     
      
          
         
-
+length= len(files)
 title=[]
 artist=[]
 album=[]                   
+title2=[]
 
 
-length=len(filenames)
+
 #from mutagen.mp3 import MP3
 #audio = MP3("Aisha.mp3")
 #print audio.info.length, audio.info.bitrate,audio.title
-#id3.getall('TTTT') == []
-m= mutagen.File(filenames[0], easy=True)
-print m['title']
-for i in range (0,length):
-    m= mutagen.File(filenames[i], easy=True)
-    #title.append(m['title'])
-    #artist.append(m['artist'])
-    #album.append(m['album'])
+#id3.getall('TIT2') == [id3['TIT2']]
+#m= mutagen.File(filenames[0], easy=True)
+for i in xrange (length):
+    m= mutagen.File(files[i], easy=True)
+    title.append(m['title'])
+    artist.append(m['artist'])
+    album.append(m['album'])
     print """
              %s 
              %s  
              %s""" %(m['title'],m['artist'],m['album'])
+ 
+             
 
 
 
